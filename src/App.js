28527c4ef692bac8aa6e754c7be825c89efa5e43
BartTheScrivner <dframe22@gmail.com>
2020-08-10 19:59:43 -0400

    added component files

--- src/App.js
+++ src/App.js
@@ -1,24 +1,14 @@
 import React from 'react';
-import logo from './logo.svg';
+import Navbar from "./components/Navbar";
+import CharSheet from './components/CharSheet';
 import './App.css';
+import FlashCard from './FlashCard';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <NavBar />
+      <CharSheet />
     </div>
   );
 }
