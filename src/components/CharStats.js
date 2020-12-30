74ae445f1ffe4e7b2c44504543ca6beba4c0a645
DevDaveFrame <dframe22@gmail.com>
2020-12-29 23:24:14 -0500

    build scaffold for CharStats

--- src/components/CharStats.js
+++ src/components/CharStats.js
@@ -1,20 +1,21 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
 
-export const CharStats = () => {
+export const CharStats = (props) => {
   return (
-    <div>
-      HELLO
+    <div id="stats-container">
+      <div id="strength"></div>
+      <div id="dexterity"></div>
+      <div id="constitution"></div>
+      <div id="wisdom"></div>
+      <div id="intelligence"></div>
+      <div id="charisma"></div>
     </div>
   )
 }
 
 const mapStateToProps = (state) => ({
-  
+  character: state.character
 })
 
-const mapDispatchToProps = {
-  
-}
-
-export default connect(mapStateToProps, mapDispatchToProps)(CharStats)
+export default connect(mapStateToProps)(CharStats)
